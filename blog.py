5d8e8436d1e5138bdf08f8fb17b6d54dc48c7a88
Prerak Raja <rajaprerak@gmail.com>
2020-05-26 22:04:24 +0530

    add edit post

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -8,7 +8,7 @@ from flask_mail import Mail
 with open('config.json','r') as f:
     params = json.load(f)["params"]
 app = Flask(__name__)
-
+app.secret_key = 'super-secret-key'
 app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = '465',
@@ -35,21 +35,78 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
+class Posts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    tagline = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(21), nullable=False)
+    content = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    img_file = db.Column(db.String(12), nullable=True)
 
 @app.route("/")
 def home():
-    return render_template('index.html',params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts)
 
 
 @app.route("/about")
 def about():
     return render_template('about.html',params=params)
 
+@app.route("/dashboard", methods=['GET','POST'])
+def dashboard():
 
-@app.route("/post")
-def post():
-    return render_template('post.html',params=params)
+    if 'user' in session and session['user'] == params["admin_user"]:
+        posts = Posts.query.all()
+        return render_template('dashboard.html', params=params,posts=posts)
+    if request.method == "POST":
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if username == params["admin_user"] and userpass == params["admin_password"]:
+            #set the session variable
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template('dashboard.html', params=params, posts = posts)
+    else:        
+        return render_template('login.html',params=params)
 
+@app.route("/post/<string:post_slug>",methods=['GET'])
+def post(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
+def edit(sno):
+    if 'user' in session and session['user'] == params["admin_user"]:
+        if request.method == 'POST':
+            title = request.form.get('title')
+            tagline = request.form.get('tagline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno =='0':
+                post = Posts(title=title,slug=slug,tagline=tagline,content=content,img_file=img_file,date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
+                post = Posts.query.filter_by(sno=sno).first()
+                post.title = title
+                post.slug = slug
+                post.tagline = tagline
+                post.content = content
+                post.img_file = img_file
+                post.date = date
+                db.session.commit()
+
+                return redirect('/edit/'+sno)
+        post = Posts.query.filter_by(sno=sno).first()
+
+        return render_template('edit.html',params=params,post=post)
+
+    return render_template
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
