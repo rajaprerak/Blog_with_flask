fc2398f93646e1c5d98b4a0de246ee4627eab38e
Prerak Raja <rajaprerak@gmail.com>
2020-05-27 14:02:07 +0530

    heroku changes

--- blog.py
+++ blog.py
@@ -162,5 +162,5 @@ def contact():
         #                   )
     return render_template('contact.html',params=params)
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
