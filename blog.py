a40a3b52a3dfd9bdbd28b30f02a6707e7bde58d5
Prerak Raja <rajaprerak@gmail.com>
2020-05-27 12:48:31 +0530

    all functionality added

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 from flask_mail import Mail
+import math
 
 
 with open('config.json','r') as f:
@@ -46,8 +47,32 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    page = request.args.get('page')
+
+    if (not str(page).isnumeric()):
+        page = 1
+
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
+
+    #Pagination 
+    #First
+    if page == 1:
+        prev = "#"
+        next = "/?page="+str(page+1)
+    #Last
+    elif page==last:
+        prev = "/?page="+str(page-1)
+        next = "#"
+    #Middle
+    else:
+        prev = "/?page="+str(page-1)
+        next = "/?page="+str(page+1)
+    
+    
+    return render_template('index.html', params=params, posts=posts, prev =prev,next=next)
 
 
 @app.route("/about")
@@ -59,7 +84,7 @@ def dashboard():
 
     if 'user' in session and session['user'] == params["admin_user"]:
         posts = Posts.query.all()
-        return render_template('dashboard.html', params=params,posts=posts)
+        return render_template('dashboard.html', params=params,posts=posts,session=session)
     if request.method == "POST":
         username = request.form.get('uname')
         userpass = request.form.get('pass')
@@ -67,9 +92,14 @@ def dashboard():
             #set the session variable
             session['user'] = username
             posts = Posts.query.all()
-            return render_template('dashboard.html', params=params, posts = posts)
+            return render_template('dashboard.html', params=params, posts = posts,session=session)
     else:        
         return render_template('login.html',params=params)
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect('/')
 
 @app.route("/post/<string:post_slug>",methods=['GET'])
 def post(post_slug):
@@ -103,10 +133,16 @@ def edit(sno):
 
                 return redirect('/edit/'+sno)
         post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html',params=params,post=post,sno=sno)
 
-        return render_template('edit.html',params=params,post=post)
+@app.route("/delete/<string:sno>", methods=['GET', 'POST'])
+def delete(sno):
+    if 'user' in session and session['user'] == params["admin_user"]:
+        post = Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
 
-    return render_template
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
